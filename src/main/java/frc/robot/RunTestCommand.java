f8e8ce66550b0f87cd6008f8e999ff1150eb1906
Paul Rensing <prensing@ligerbots.org>
2022-02-10 09:56:11 -0500

    Remove OldCmd dependency, some formatting cleanup

--- src/main/java/frc/robot/RunTestCommand.java
+++ src/main/java/frc/robot/RunTestCommand.java
@@ -1,38 +1,36 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class RunTestCommand extends CommandBase {
-    
+
     TestBench testBench;
 
-    RunTestCommand(TestBench testBench){
+    RunTestCommand(TestBench testBench) {
         this.testBench = testBench;
         System.out.println("testbench constructed");
     }
 
     @Override
     public void initialize() {
-      System.out.println("initialized");
+        System.out.println("initialized");
     }
-  
+
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-      testBench.runTestBench();
-      System.out.println("runTestBench");
+        testBench.runTestBench();
+        System.out.println("runTestBench");
     }
-  
+
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-
     }
-  
+
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-      return false;
+        return false;
     }
 }
