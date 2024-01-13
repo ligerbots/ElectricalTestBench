9d6797cf3a97b19c6575c4f9603ffaae0982745a
Paul Rensing <prensing@ligerbots.org>
2024-01-13 18:12:46 -0500

    Use arrays of motors

--- src/main/java/frc/robot/TestBench.java
+++ src/main/java/frc/robot/TestBench.java
@@ -9,76 +9,48 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class TestBench {
+    static final int NUM_NEOS = 8;
+    static final int NUM_FALCONS = 4;
+
     // declaring the Neos
-    private CANSparkMax neo1 = new CANSparkMax(1, MotorType.kBrushless);
-    private CANSparkMax neo2 = new CANSparkMax(2, MotorType.kBrushless); 
-    private CANSparkMax neo3 = new CANSparkMax(3, MotorType.kBrushless);
-    private CANSparkMax neo4 = new CANSparkMax(4, MotorType.kBrushless);
-    private CANSparkMax neo5 = new CANSparkMax(5, MotorType.kBrushless);
-    private CANSparkMax neo6 = new CANSparkMax(6, MotorType.kBrushless);
-    private CANSparkMax neo7 = new CANSparkMax(7, MotorType.kBrushless);
-    private CANSparkMax neo8 = new CANSparkMax(8, MotorType.kBrushless);
+    private CANSparkMax[] neos = new CANSparkMax[ NUM_NEOS ];
  
     // declaring the Falcons
-    private TalonFX talon1 = new TalonFX(1);
-    private TalonFX talon2 = new TalonFX(2); 
-    private TalonFX talon3 = new TalonFX(3);
-    private TalonFX talon4 = new TalonFX(4);
-    private TalonFX talon5 = new TalonFX(5);
-    private TalonFX talon6 = new TalonFX(6);
+    private TalonFX[] falcons = new TalonFX[ NUM_FALCONS ];
 
     public TestBench(){
-        
-        SmartDashboard.putNumber("NEO 1 Volt", 0);
-        SmartDashboard.putNumber("NEO 2 Volt", 0);
-        SmartDashboard.putNumber("NEO 3 Volt", 0);
-        SmartDashboard.putNumber("NEO 4 Volt", 0);
-        SmartDashboard.putNumber("NEO 5 Volt", 0);
-        SmartDashboard.putNumber("NEO 6 Volt", 0);
-        SmartDashboard.putNumber("NEO 7 Volt", 0);
-        SmartDashboard.putNumber("NEO 8 Volt", 0);
+        for (int iN=0; iN < NUM_NEOS; iN++) {
+            // CAN IDs should start at 1
+            int canId = iN + 1;
+            neos[iN] = new CANSparkMax(canId, MotorType.kBrushless);
+            SmartDashboard.putNumber("NEO " + canId + " Volt", 0);
+            SmartDashboard.putNumber("NEO " + canId + " Faults", 0);
+            // SmartDashboard.putNumber("NEO " + canId + " RPM", 0);
+        }
 
-        SmartDashboard.putNumber("NEO 1 Faults", 0);
-        SmartDashboard.putNumber("NEO 2 Faults", 0);
-        SmartDashboard.putNumber("NEO 3 Faults", 0);
-        SmartDashboard.putNumber("NEO 4 Faults", 0);
-        SmartDashboard.putNumber("NEO 5 Faults", 0);
-        SmartDashboard.putNumber("NEO 6 Faults", 0);
-        SmartDashboard.putNumber("NEO 7 Faults", 0);
-        SmartDashboard.putNumber("NEO 8 Faults", 0);
-
-        SmartDashboard.putNumber("Falcon 1 Volt", 0);
-        SmartDashboard.putNumber("Falcon 2 Volt", 0);
-        SmartDashboard.putNumber("Falcon 3 Volt", 0);
-        SmartDashboard.putNumber("Falcon 4 Volt", 0);
-        SmartDashboard.putNumber("Falcon 5 Volt", 0);
-        SmartDashboard.putNumber("Falcon 6 Volt", 0);
+        for (int iF=0; iF < NUM_FALCONS; iF++) {
+            // CAN IDs should start at 1
+            int canId = iF + 1;
+            falcons[iF] = new TalonFX(canId); 
+            SmartDashboard.putNumber("Falcon " + canId + " Volt", 0);
+            // SmartDashboard.putNumber("Falcon " + canId + " RPM", 0);
+        }
     }
 
     public void runTestBench(){
-        neo1.setVoltage(SmartDashboard.getNumber("NEO 1 Volt", 0));
-        neo2.setVoltage(SmartDashboard.getNumber("NEO 2 Volt", 0));
-        neo3.setVoltage(SmartDashboard.getNumber("NEO 3 Volt", 0));
-        neo4.setVoltage(SmartDashboard.getNumber("NEO 4 Volt", 0));
-        neo5.setVoltage(SmartDashboard.getNumber("NEO 5 Volt", 0));
-        neo6.setVoltage(SmartDashboard.getNumber("NEO 6 Volt", 0));
-        neo7.setVoltage(SmartDashboard.getNumber("NEO 7 Volt", 0));
-        neo8.setVoltage(SmartDashboard.getNumber("NEO 8 Volt", 0));
+        for (int iN=0; iN < NUM_NEOS; iN++) {
+            // CAN IDs should start at 1
+            int canId = iN + 1;
+            neos[iN].setVoltage(SmartDashboard.getNumber("NEO " + canId + " Volt", 0));
+            SmartDashboard.putNumber("NEO " + canId + " Faults", neos[iN].getFaults());
+            // SmartDashboard.putNumber("NEO " + canId + " RPM", 0);
+        }
 
-        SmartDashboard.putNumber("NEO 1 Faults", neo1.getFaults());
-        SmartDashboard.putNumber("NEO 2 Faults", neo2.getFaults());
-        SmartDashboard.putNumber("NEO 3 Faults", neo3.getFaults());
-        SmartDashboard.putNumber("NEO 4 Faults", neo4.getFaults());
-        SmartDashboard.putNumber("NEO 5 Faults", neo5.getFaults());
-        SmartDashboard.putNumber("NEO 6 Faults", neo6.getFaults());
-        SmartDashboard.putNumber("NEO 7 Faults", neo7.getFaults());
-        SmartDashboard.putNumber("NEO 8 Faults", neo8.getFaults());
-
-        talon1.set(ControlMode.PercentOutput, SmartDashboard.getNumber("Falcon 1 Volt", 0) / 12.0);
-        talon2.set(ControlMode.PercentOutput, SmartDashboard.getNumber("Falcon 2 Volt", 0) / 12.0);
-        talon3.set(ControlMode.PercentOutput, SmartDashboard.getNumber("Falcon 3 Volt", 0) / 12.0);
-        talon4.set(ControlMode.PercentOutput, SmartDashboard.getNumber("Falcon 4 Volt", 0) / 12.0);
-        talon5.set(ControlMode.PercentOutput, SmartDashboard.getNumber("Falcon 5 Volt", 0) / 12.0);
-        talon6.set(ControlMode.PercentOutput, SmartDashboard.getNumber("Falcon 6 Volt", 0) / 12.0);
+        for (int iF=0; iF < NUM_FALCONS; iF++) {
+            // CAN IDs should start at 1
+            int canId = iF + 1;
+            falcons[iF].set(ControlMode.PercentOutput, SmartDashboard.getNumber("Falcon " + canId + " Volt", 0) / 12.0);
+            // SmartDashboard.putNumber("Falcon " + canId + " RPM", 0);
+        }
     }
 }
